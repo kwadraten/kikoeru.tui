9357bb54a4b5a13e784f047589e37d6089b594d1
squaresum <schild0van0man@duck.com>
2026-05-17 21:33:32 +0800

    use built-in FigletText

--- Views/WelcomeView.fs
+++ Views/WelcomeView.fs
@@ -8,17 +8,11 @@ let render () =
     let centeredText text =
         AlignWidget(TextBlockWidget text, Alignment.Center).Fill() :> Hex1bWidget
 
-    let logo =
-        [| "   __    _  __                           __         _ "
-           "  / /__ (_)/ /__ ___  ___  ____ __ __   / /_ __ __ (_)"
-           " /  '_// //  '_// _ \/ -_)/ __// // /_ / __// // // / "
-           "/_/\_\/_//_/\_\ \___/\__//_/   \_,_/(_)\__/ \_,_//_/  " |]
-
     seq {
         AlignWidget(
             VStackWidget
                 [| centeredText "A Terminal Player for Kikoeru/Neokikoeru Backend."
-                   yield! [| for t in logo -> centeredText t |]
+                   FigletTextWidget("kikoeru.tui").Font(FigletFonts.Slant).Layout(FigletLayoutMode.Smushed)
                    centeredText (sprintf "当前服务器：%s" (serverDisplayName ()))
                    playbackStatusText globalState.PlaybackStatus |> centeredText |],
             Alignment.Center
